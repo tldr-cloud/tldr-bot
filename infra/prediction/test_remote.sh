f2b1bba4b67d61bfc08ab805f6801e866af1f772
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2020-07-07 11:54:56 -0700

    new model v5 uploaded, just to trigger re-pull of the docker container

--- infra/prediction/test_remote.sh
+++ infra/prediction/test_remote.sh
@@ -7,5 +7,5 @@ PROJECT_NAME="tldr-278619"
 
 curl -X POST -k -H "Content-Type: application/json" \
     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
-    "${ENDPOINT}/projects/${PROJECT_NAME}/models/${MODEL_NAME}/versions/${VERSION_NAME}:predict" \
+    "${ENDPOINT}/projects/${PROJECT_NAME}/models/${MODEL_NAME}:predict" \
     -d @request.json
