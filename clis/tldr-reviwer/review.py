141b59acc891e56d1a8d6998a1fd4b92c6980662
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2020-07-29 21:01:12 -0700

    First working version of voice bot for Google Assistant.

--- clis/tldr-reviwer/review.py
+++ clis/tldr-reviwer/review.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 from google.cloud import firestore
 
--- functions/tldr-voice-bot/main.py
+++ functions/tldr-voice-bot/main.py
@@ -1,22 +1,35 @@
 import json
+import datetime
+
+from google.cloud import firestore
+
+urls_collection = firestore.Client().collection(u"urls")
 
 
 def process_call(request):
-    bot_resp = {
-      "payload": {
-        "google": {
-          "expectUserResponse": True,
-          "richResponse": {
-            "items": [
-              {
-                "simpleResponse": {
-                  "textToSpeech": "news one"
-                }
-              }
-            ]
-          }
+  today = datetime.datetime.now()
+  delta = datetime.timedelta(days=2)
+  target = today - delta
+  docs = urls_collection.where(u"published", u"==", True).where("date", ">", target).stream()
+  titles = []
+  for doc in docs:
+    print("doc found: {}".format(str(doc.id)))
+    titles.append(doc.get("title"))
+
+  items = [{"simpleResponse": {"textToSpeech": title}} for title in titles]
+
+  bot_resp = {
+    "payload": {
+      "google": {
+        "expectUserResponse": True,
+        "richResponse": {
+          "items": items
         }
       }
     }
+  }
 
-    return json.dumps(bot_resp)
+  return json.dumps(bot_resp)
+
+if __name__ == "__main__":
+  print(process_call(None))
