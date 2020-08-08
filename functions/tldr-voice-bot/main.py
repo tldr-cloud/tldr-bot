7f8200f1edbeb18aee7bbcea71115318ddba5e4f
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2020-08-08 14:04:01 -0700

    Small stile refactoring and updated to the voice response.

--- functions/tldr-voice-bot/main.py
+++ functions/tldr-voice-bot/main.py
@@ -7,36 +7,37 @@ urls_collection = firestore.Client().collection(u"urls")
 
 
 def process_call(request):
-  today = datetime.datetime.now()
-  delta = datetime.timedelta(days=2)
-  target = today - delta
-  docs = urls_collection.where(u"published", u"==", True).where("date", ">", target).stream()
-  titles = []
-  for doc in docs:
-    print("doc found: {}".format(str(doc.id)))
-    titles.append(doc.get("title"))
+    today = datetime.datetime.now()
+    delta = datetime.timedelta(days=2)
+    target = today - delta
+    docs = urls_collection.where(u"published", u"==", True).where("date", ">", target).stream()
+    titles = []
+    for doc in docs:
+        print("doc found: {}".format(str(doc.id)))
+        titles.append(doc.get("title"))
 
-  final_text = ". ".join(titles)
+    news_list = ". ".join(titles)
+    final_text = "Here are all the news for the last day: {}".format(news_list)
 
-  bot_resp = {
-    "payload": {
-      "google": {
-        "expectUserResponse": False,
-        "richResponse": {
-          "items": [
-            {
-              "simpleResponse": {
-                "textToSpeech": final_text
-              }
+    bot_resp = {
+        "payload": {
+            "google": {
+                "expectUserResponse": False,
+                "richResponse": {
+                    "items": [
+                        {
+                            "simpleResponse": {
+                                "textToSpeech": final_text
+                            }
+                        }
+                    ]
+                }
             }
-          ]
         }
-      }
     }
-  }
 
-  return json.dumps(bot_resp)
+    return json.dumps(bot_resp)
 
 
 if __name__ == "__main__":
-  print(process_call(None))
+    print(process_call(None))
